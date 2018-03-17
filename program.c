e9fa6957c024694fd4c72c59948e9bbc9c52d2f1
AKame <37278780+akame31@users.noreply.github.com>
2018-03-17 17:55:55 +0000

    Update program.c

--- program.c
+++ program.c
@@ -7,7 +7,7 @@ int main(void) {
 
 	printf("Olá, deseja resolver o erro? Se sim, digiter enter ");
 	getchar();
-	remove("/var/cache/apt/archives/lock /var/cache/apt/archives/lock_bak");
+	remove("/var/cache/apt/archives/lock");
 
 	printf("Pronto, agora depois de terminado o programa, digite sudo apt-get install _nomedopacote_ \n");
 
